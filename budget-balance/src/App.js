82353848ab4d041369064a8e9fd965695fde4038
hassaanali723 <hassaanali723@gmail.com>
2023-02-16 16:52:52 +0500

    setup file structure and add files

--- budget-balance/src/App.js
+++ budget-balance/src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Expenses from "./pages/Expenses";
+import Savings from "./pages/Savings";
+import Transactions from "./pages/Transactions";
+import Budget from "./pages/Budget";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return <div className="App">Budget Balance</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/savings" element={<Savings />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/budget" element={<Budget />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
