41c8d3e2ff2ed2fcbe4e1cf6234fd111969b8b7e
sarahfarrukh <sarahfarrukh18@gmail.com>
2023-02-17 12:41:50 +0500

    login

--- budget-balance/src/api/auth/authApi.js
+++ budget-balance/src/api/auth/authApi.js
@@ -5,3 +5,12 @@ const baseUrl = "http://localhost:5000";
 export const createUser = (user) => {
   return axios.post(`${baseUrl}/users/register`, user);
 };
+
+export const getUser = async (user) => {
+  try {
+    const response = await axios.post(`${baseUrl}/users/login`, user);
+    return response.data;
+  } catch (error) {
+    throw new Error('Login failed');
+  }
+}
